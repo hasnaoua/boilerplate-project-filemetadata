930acf24809ac03bdcb9382240b0abdda6b7f093
hasnaoua <abdelhayhasnaoui@gmail.com>
2025-07-26 16:26:03 +0200

    add file upmoad endpoint

--- index.js
+++ index.js
@@ -1,14 +1,46 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require('multer');
 require('dotenv').config()
 
 var app = express();
+
+// Set up storage engine
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/'); // Folder to save uploaded files
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '-' + file.originalname); // Rename to avoid conflicts
+  }
+});
+
+// Initialize upload
+const upload = multer({ storage: storage });
+
+// Make uploads folder accessible
+app.use('/uploads', express.static('uploads'));
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  console.log(req)
+  const file = req.file;
+
+  if (!file) {
+    return res.status(400).json({ error: 'No file uploaded' });
+  }
+
+  res.json({
+    name: file.originalname,
+    type: file.mimetype,
+    size: file.size
+  });
 });
 
 
